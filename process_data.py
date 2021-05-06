20f6b3ff7e32dee6d5057eef1ac69da5d1d0aae7
Richard Morello <richard@sneakerfish.com>
2021-05-06 12:54:59 -0400

    Added Spacy as a UDF
    This was quite a pain.  Created a user defined function to
    call Spacy on a column in a Spark Data Frame.  Basically used
    the helpful comments here, but with some changes:
    
    https://stackoverflow.com/questions/50880303/python-pickle-spacy-for-pyspark

--- process_data.py
+++ process_data.py
@@ -1,12 +1,28 @@
-# problem2.py
+# process_data.py
 # intended to be submitted via pyspark but produce results on the command line.
 
+import os, spacy, en_core_web_lg
 from pyspark import SparkConf, SparkContext
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType, StructField, StringType, DoubleType, IntegerType, LongType
-from pyspark.sql.functions import expr, col, column
+from pyspark.sql.types import StructType, StructField, StringType, DoubleType, IntegerType, LongType, ArrayType
+from pyspark.sql.functions import expr, col, column, udf
 
 spark = SparkSession.builder.appName("process_data").getOrCreate()
+
+nlp = spacy.load("en_core_web_lg")
+
+def get_entities_udf():
+    def get_entities(text):
+        global nlp
+        try:
+            doc = nlp(text)
+        except:
+            nlp = spacy.load('en_core_web_lg')
+            doc = nlp(text)
+        return [t.text for t in doc.ents if t.label_ in ["ORG", "PRODUCT", "PERSON", "WORK_OF_ART", "GPE"]]
+    res_udf = udf(get_entities, ArrayType(StringType()))
+    return res_udf
+
 
 news_schema = schema = StructType([
     StructField("text", StringType(), False),
@@ -14,6 +30,14 @@ news_schema = schema = StructType([
     StructField("authors", StringType(), False),
     StructField("url", StringType(), False)
     ])
+
+
 df1 = spark.read.schema(news_schema).json("rawdata/cnn.com")
-df1.printSchema()
-df1.show()
+df2 = df1.withColumn("entities", get_entities_udf()(df1.text))
+print(df2.take(1))
+
+
+# df2.printSchema()
+# df2.show()
+
+print(entities("This is a test of Mr Spock goes to Washington in February, 2002."))
